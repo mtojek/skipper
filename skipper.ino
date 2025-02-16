7f29d4ffedc06dafe34744428cb76a661183de9e
Marcin Tojek <marcin@coder.com>
2025-02-16 14:08:07 +0100

    Audio working

--- skipper.ino
+++ skipper.ino
@@ -1,9 +1,20 @@
 #include <Adafruit_NeoPixel.h>
 #include <LovyanGFX.hpp>
 #include <SD.h>
+#include <SPIFFS.h>
+
+#include "AudioFileSourceSD.h"
+#include "AudioFileSourceBuffer.h"
+#include "AudioFileSourceID3.h"
+#include "AudioGeneratorMP3.h"
+#include "AudioOutputI2S.h"
 
 #define LED_PIN 38
 #define SD_CS 7
+
+#define I2S_LRC 42   // Left-right clock (LRCLK)
+#define I2S_BCLK 41  // Bit clock (BCLK)
+#define I2S_DOUT 40  // Data output to MAX98357A (DIN)
 
 Adafruit_NeoPixel pixel = Adafruit_NeoPixel(1, LED_PIN, NEO_GRB + NEO_KHZ800);
 
@@ -19,7 +30,7 @@ public:
 
       cfg.spi_host = SPI2_HOST;
       cfg.spi_mode = 0;
-      cfg.freq_write = 40000000;
+      cfg.freq_write = 48000000;
       cfg.use_lock = true;
       cfg.dma_channel = SPI_DMA_CH_AUTO;
       cfg.pin_dc = 46;
@@ -71,6 +82,45 @@ public:
 LGFX display;
 static File root;
 
+AudioGeneratorMP3 *mp3;
+AudioFileSourceSD *file;
+AudioFileSourceBuffer *buff;
+AudioFileSourceID3 *id3;
+AudioOutputI2S *out;
+
+void MDCallback(void *cbData, const char *type, bool isUnicode, const char *string) {
+  (void)cbData;
+  Serial.printf("ID3 callback for: %s = '", type);
+
+  if (isUnicode) {
+    string += 2;
+  }
+
+  while (*string) {
+    char a = *(string++);
+    if (isUnicode) {
+      string++;
+    }
+    Serial.printf("%c", a);
+  }
+  Serial.printf("'\n");
+  Serial.flush();
+}
+
+// Called when there's a warning or error (like a buffer underflow or decode hiccup)
+void StatusCallback(void *cbData, int code, const char *string)
+{
+  const char *ptr = reinterpret_cast<const char *>(cbData);
+  // Note that the string may be in PROGMEM, so copy it to RAM for printf
+  char s1[64];
+  strncpy_P(s1, string, sizeof(s1));
+  s1[sizeof(s1)-1]=0;
+  Serial.printf("STATUS(%s) '%d' = '%s'\n", ptr, code, s1);
+  Serial.flush();
+}
+
+unsigned long lastImageChange = 0;
+
 void setup() {
   Serial.begin(115200);
   while (!Serial) {}
@@ -99,7 +149,7 @@ void setup() {
   }
 
   display.drawString("SD OK!", 5, 40);
- 
+
   Serial.print("Card type: ");
   switch (SD.cardType()) {
     case CARD_NONE: Serial.println("No SD card attached"); break;
@@ -108,26 +158,62 @@ void setup() {
     case CARD_SDHC: Serial.println("SDHC"); break;
     default: Serial.println("Unknown");
   }
-  
+
   Serial.print("Card size: ");
   Serial.print(SD.cardSize() / (1024 * 1024));
   Serial.println(" MB");
 
   root = SD.open("/");
 
+  // Audio
+  audioLogger = &Serial;
+
+  // Set up I2S output
+  out = new AudioOutputI2S(0, 0, 128);
+  out->SetPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
+  out->SetGain(0.1);
+  out->SetOutputModeMono(true);
+
+  // Open the MP3 file from SD
+  file = new AudioFileSourceSD("/music2.mp3");
+  buff = new AudioFileSourceBuffer(file, 4096);
+  buff->RegisterStatusCB(StatusCallback, (void*)"buffer");
+  id3 = new AudioFileSourceID3(buff);
+  id3->RegisterMetadataCB(MDCallback, (void *)"ID3TAG");
+
+  void *space = malloc(29192);
+  mp3 = new AudioGeneratorMP3(space, 29192);
+  mp3->begin(id3, out);
+
   // Done!
   Serial.println("Hello from Arduino!");
 }
 
+void loop() {
+  if (mp3->isRunning()) {
+    if (!mp3->loop()) mp3->stop();
+  } else {
+    Serial.printf("MP3 done\n");
+    delay(1000);
+    return;
+  }
+
+  rotateImage();
+}
+
 static File entry;
 
-void loop() {
+void rotateImage() {
+  if (lastImageChange != 0 && millis() - lastImageChange < 10000) {
+    return;
+  }
+
   entry = root.openNextFile();
   if (!entry) {
     root.rewindDirectory();
-    return;  
+    return;
   }
-  
+
   if (String(entry.name()).startsWith("._")) {
     return;
   }
@@ -139,7 +225,8 @@ void loop() {
   String filename = String("/") + entry.name();
   drawBMP(filename.c_str(), 0, 0);
   display.drawString(filename, 5, 8);
-  delay(1000);
+
+  lastImageChange = millis();
 }
 
 // Function to load BMP images from SD card
@@ -150,8 +237,6 @@ void drawBMP(const char *filename, int16_t x, int16_t y) {
     Serial.println(filename);
     return;
   }
-
   display.drawBmp(&file, x, y);
   file.close();
 }
-
